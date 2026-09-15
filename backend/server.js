9caaf111dbdd63fcf6ff635293d7ab35d6227aff
CJH <ziryo07247@gmail.com>
2026-09-15 10:50:33 +0900

    프론트/ 백엔드 서버 기초 연결2

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,11 @@ app.use(
 );
 app.use(express.json());
 
+// 🟢 이 테스트 주소를 추가합니다!
+app.get("/api/test", (req, res) => {
+  res.json({ message: "프론트엔드와 백엔드 연결 성공!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Node.js 서버 실행 중: http://localhost:${PORT}`);
 });
